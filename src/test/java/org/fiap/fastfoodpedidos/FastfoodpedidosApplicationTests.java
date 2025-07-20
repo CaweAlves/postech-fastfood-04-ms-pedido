c73d099d7771d30903d990ac5ffef7b5f6682db5
CaweAlves <contato@cawe.dev>
2025-07-20 12:37:57 -0300

    feat: integrando sonarqube

--- src/test/java/org/fiap/fastfoodpedidos/FastfoodpedidosApplicationTests.java
+++ src/test/java/org/fiap/fastfoodpedidos/FastfoodpedidosApplicationTests.java
@@ -2,12 +2,37 @@ package org.fiap.fastfoodpedidos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class FastfoodpedidosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:14").withDatabaseName("testdb").withUsername("sa").withPassword("password");
 
+    @Container
+    static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse("localstack/localstack:latest-amd64")).withServices(LocalStackContainer.Service.SQS);
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+
+        registry.add("spring.cloud.aws.endpoint", () -> localStackContainer.getEndpointOverride(LocalStackContainer.Service.SQS).toString());
+        registry.add("spring.cloud.aws.credentials.access-key", localStackContainer::getAccessKey);
+        registry.add("spring.cloud.aws.credentials.secret-key", localStackContainer::getSecretKey);
+        registry.add("spring.cloud.aws.region.static", localStackContainer::getRegion);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
